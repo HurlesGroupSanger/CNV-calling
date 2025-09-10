e42f7a84260e5e0bbbbb154faabd38523e5a3257
Petr Danecek <pd3@sanger.ac.uk>
2025-09-10 09:20:55 +0100

    Add scripts

--- install.sh
+++ install.sh
@@ -60,21 +60,35 @@ if [ "$?" != "0" ]; then
     echo 'export PERL5LIB='$dst_dir'/vr-runner/modules:$PERL5LIB' >> $dst_dir/setenv.sh
 fi
 
+# - run-convex pipeline
+run-convex +sampleconf 2>/dev/null
+if [ "$?" != "111" ]; then
+    if [ ! -e CoNVex/utils/run-convex ]; then
+        git clone git@github.com:HurlesGroupSanger/CoNVex.git
+    fi
+    echo 'export PATH='$dst_dir'/CoNVex/utils:$PATH' >> $dst_dir/setenv.sh
+fi
+
 # - samtools, annot-tsv
 version=1.22
-samtools --version >/dev/null 2>&1
-if [ "$?" != "0" ]; then
+samtools  --version >/dev/null 2>&1; has_samtools=$(( $? != 0 ))
+annot-tsv --version >/dev/null 2>&1; has_annot_tsv=$(( $? != 0 ))
+if [ "$has_samtools" -ne 0 ] || [ "$has_annot_tsv" -ne 0 ]; then
     if [ ! -e samtools-$version/samtools ]; then
         echo "need samtools"
         wget https://github.com/samtools/samtools/releases/download/$version/samtools-$version.tar.bz2
         tar xjf samtools-$version.tar.bz2
         pushd samtools-$version
         set -e
+        cd htslib-$version
+        ./configure && make -j
+        cd ..
         ./configure && make -j
         set +e
         popd
     fi
     echo 'export PATH='$dst_dir'/samtools-'$version':$PATH' >> $dst_dir/setenv.sh
+    echo 'export PATH='$dst_dir'/samtools-'$version'/htslib-'$version':$PATH' >> $dst_dir/setenv.sh
 fi
 
 
